3c6977a2436acbdb4932bdf8aa3b0c21926b621a
Santiago Briceño <83045233+SantiagoBriceno@users.noreply.github.com>
2026-02-02 17:26:17 -0400

    fix: add mock store

--- frontend/src/app/views/home/edit-recipe/edit-recipe.spec.ts
+++ frontend/src/app/views/home/edit-recipe/edit-recipe.spec.ts
@@ -1,24 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditRecipe } from './edit-recipe';
-import { provideMockStore } from '@ngrx/store/testing';
-import { provideRouter } from '@angular/router';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { ActivatedRoute, provideRouter } from '@angular/router';
 
 describe('EditRecipe', () => {
   let component: EditRecipe;
   let fixture: ComponentFixture<EditRecipe>;
+  let store: MockStore;
+  let initialState = {
+    recipe: {
+      selectedRecipe: null,
+    },
+    collab: {
+      recipeToEdit: null,
+      inputsOccupied: {},
+      usersInRoom: [],
+    },
+    auth: {
+      username: 'testUser',
+    },
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [EditRecipe],
       providers: [
-        provideMockStore({
-          initialState: {
-            collab: {
-              recipeToEdit: null,
+        provideMockStore({ initialState }),
+        provideRouter([{ path: 'edit-recipe/:id', component: EditRecipe }]),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: () => {
+                  return 'edit-recipe/123';
+                },
+              },
             },
           },
-        }),
-        provideRouter([{ path: 'edit-recipe/:id', component: EditRecipe }]),
+        }
       ],
     }).compileComponents();
 
